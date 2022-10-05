e1a43a898840b3961c04686d908d08357166ca19
alizaynoune <zaynoune.ali@gmail.com>
2022-10-05 23:30:05 +0100

    fix error prfile/user not found
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, Req, Res } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { Response, Request, query } from 'express';
+import { Response, Request } from 'express';
 
 @Injectable()
 export class AppService {
@@ -10,21 +10,22 @@ export class AppService {
   }
 
   // get profile
-  async profile(req: Request,res: Response, params: any) {
+  async profile(req: Request, res: Response, params: any) {
     try {
-      const slector = params.username? {username: params.username} : {intra_id: req.user.sub}
+      const slector = params.username
+        ? { username: params.username }
+        : { intra_id: req.user.sub };
       const data = await this.prisma.users.findUnique({
         where: {
-          ...slector
-      },
-        include: {users_achievements: {select: {achievements: true } }},
-      })
-      const ach = data.users_achievements.map(e => {
-        return(Object.values(e)[0])
-        
-      })
-      const ret = {...data, users_achievements: ach}
-      
+          ...slector,
+        },
+        include: { users_achievements: { select: { achievements: true } } },
+      });
+      if (!data) throw 'user not found';
+      const ach = data.users_achievements.map((e) => {
+        return Object.values(e)[0];
+      });
+      const ret = { ...data, users_achievements: ach };
       return res.status(200).json(ret);
     } catch (error) {
       return res.status(400).json({
