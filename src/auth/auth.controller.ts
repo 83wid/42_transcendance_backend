fa830f8671337cf390b800aef36ee844b0fe8d0f
alizaynoune <zaynoune.ali@gmail.com>
2022-10-30 22:52:29 +0100

    fix updateAvatarCover
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,28 @@
-import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { AuthService } from './auth.service';
-import { FortyTwoAuthGuard } from './fortyTwo-auth.guard';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+import { FortyTwoAuthGuard } from "./fortyTwo-auth.guard";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('/login')
+  @Get("/login")
   @UseGuards(FortyTwoAuthGuard)
   async Auth() {
     //return req.query
   }
-  @Get('/42/callback')
+  @Get("/42/callback")
   @UseGuards(FortyTwoAuthGuard)
   async Callback(@Req() req: Request, @Res() res: Response) {
     const token = await this.authService.authenticate(req);
 
-    return res.redirect(`http://localhost:3000?token=${token}`);
+    // return res.redirect(`http://localhost:3000?token=${token}`);
+    return res.redirect(`${req.headers.referer}?token=${token}`);
   }
 
-  @Post('/me')
+  @Post("/me")
   @UseGuards(JwtAuthGuard)
   async Login(@Req() req: Request, @Res() res: Response) {
     return await this.authService.authMe(req.user.sub, res);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Put, Req, Res, Query, UseGuards, UseInterceptors, UploadedFiles } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param,
+  Put,
+  Req,
+  Res,
+  Query,
+  UseGuards,
+  UseInterceptors,
+  UploadedFiles,
+} from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { UsersService } from "./users.service";
 import { Request, Response } from "express";
@@ -48,10 +59,10 @@ export class UsersController {
     const { avatar, cover } = files;
     // http://localhost:3000/upload/avatar_51111.jpeg
     console.log(avatar);
-    
+
     let data = req.body;
-    if (avatar) data.img_url = `users/image/${avatar[0].filename}`;
-    if (cover) data.cover = `users/image/${cover[0].filename}`;
+    if (avatar) data.img_url = `${process.env.PUBLIC_URL}/users/image/${avatar[0].filename}`;
+    if (cover) data.cover = `${process.env.PUBLIC_URL}/users/image/${cover[0].filename}`;
     console.log(data);
 
     const user = await this.usersService.updateUser({
